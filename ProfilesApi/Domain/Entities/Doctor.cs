900ed3b9b31b32e5cdfa2fbd75123cb01db8738c
auenaswaichik <ilpeshkur@gmail.com>
2025-06-19 16:41:09 +0300

    Adding patient, doctor and admin entities in domain project

---
+++ ProfilesApi/Domain/Entities/Doctor.cs
@@ -0,0 +1,14 @@
+namespace Domain.Entities;
+
+public class Doctor
+{
+    public Guid Doctor_ID { get; set; }
+    public string? DoctorFirstName { get; set; }
+    public string? DoctorMiddleName { get; set; }
+    public string? DoctorLastName { get; set; }
+    public DateTime DoctorDateBirth { get; set; }
+    public DateOnly DoctorCareerStartYear { get; set; }
+    public Guid Profile_ID { get; set; }
+    public Guid Specialization_ID { get; set; }
+    public Guid Office_ID { get; set; }
+}
