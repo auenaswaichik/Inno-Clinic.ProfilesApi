616f4751c273fe51833d3f4813d790dd8ea3fbd6
auenaswaichik <ilpeshkur@gmail.com>
2025-06-19 16:08:14 +0300

    Adding swagger to UI project

--- ProfilesApi/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ ProfilesApi/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93a2d7c6692300c61f46f72705475ec10fda99c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb8e162bb06321f537ce5922bcc7c969e0dde18f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProfilesApi/UI/Program.cs
+++ ProfilesApi/UI/Program.cs
@@ -1,12 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
