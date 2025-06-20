4acd063c555fb4557164e281cfe3941b37f6e640
auenaswaichik <ilpeshkur@gmail.com>
2025-06-20 10:42:01 +0300

    Adding IRepositoryManager for managing repositories to application layer

--- ProfilesApi/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ ProfilesApi/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8c6476cb8f73d13fa941607815afcc6355def3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f6490a1ea230b3ebb0595925b105ff9219daf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProfilesApi/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ ProfilesApi/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852add4e8179bb72c46f4a13b69dcd9add809133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f6490a1ea230b3ebb0595925b105ff9219daf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
