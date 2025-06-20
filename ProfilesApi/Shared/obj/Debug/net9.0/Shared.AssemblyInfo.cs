2792687f27a6e299198894e56c5f1cf049ac70c0
auenaswaichik <ilpeshkur@gmail.com>
2025-06-20 09:25:43 +0300

    Adding ProfileApiDbContext to services in UI with options, described in .env file

--- ProfilesApi/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ ProfilesApi/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8fbdb8f1b0316b814b4514e783ebb729f30d4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505cb2c0015ca67d4faa9edd6a05cebb57e09bd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
