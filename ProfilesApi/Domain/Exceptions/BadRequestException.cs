e24617aad0be9b9835d429aa96f4546968f12faa
auenaswaichik <ilpeshkur@gmail.com>
2025-07-01 14:20:31 +0300

    Edditing custom exceptions

--- ProfilesApi/Domain/Exceptions/BadRequestException.cs
+++ ProfilesApi/Domain/Exceptions/BadRequestException.cs
@@ -1,16 +1,8 @@
 namespace Domain.Exceptions;
 
-[Serializable]
 public class BadRequestException : Exception
 {
-    public BadRequestException ()
-    {}
-
     public BadRequestException (string message) 
         : base(message)
     {}
-
-    public BadRequestException (string message, Exception innerException)
-        : base (message, innerException)
-    {}    
 }
