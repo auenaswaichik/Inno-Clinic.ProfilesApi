b79ff9e02f7df15903560cb0a6a2f057eaf84813
auenaswaichik <ilpeshkur@gmail.com>
2025-07-03 15:07:01 +0300

    Adding update patient command

---
+++ ProfilesApi/Application/Patients/UseCases/UpdatePatient/UpdatePatientCommandHandler.cs
@@ -0,0 +1,59 @@
+using System.Text;
+using Application.Patients.Models;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces.IRepositories;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Patients.UseCases.UpdatePatient;
+
+public class UpdatePatientCommandHandler : IRequestHandler<UpdatePatientCommand, PatientDTO>
+{
+    private readonly IPatientRepository _patientRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<UpdatePatientCommand> _validator;
+
+    public UpdatePatientCommandHandler(IPatientRepository patientRepository, IUnitOfWork unitOfWork, IValidator<UpdatePatientCommand> validator)
+    {
+        _patientRepository = patientRepository;
+        _unitOfWork = unitOfWork;
+        _validator = validator;
+    }
+
+    public async Task<PatientDTO> Handle(UpdatePatientCommand request, CancellationToken token)
+    {
+        var validationResult = _validator.Validate(request);
+
+        if (!validationResult.IsValid)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var error in validationResult.Errors)
+            {
+                stringBuilder.AppendLine(error.ErrorMessage);
+            }
+
+            throw new BadRequestException(stringBuilder.ToString());
+        }
+
+        var patient = new Patient()
+        {
+            Id = request.Id,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            DateBirth = request.DateBirth,
+            ProfileId = request.ProfileId
+        };
+
+        var updatedPatient = _patientRepository.Update(patient);
+        await _unitOfWork.SaveAsync();
+
+        return new PatientDTO(
+                    updatedPatient.FirstName,
+                    updatedPatient.LastName,
+                    updatedPatient.DateBirth,
+                    updatedPatient.ProfileId
+                );
+    }
+}
