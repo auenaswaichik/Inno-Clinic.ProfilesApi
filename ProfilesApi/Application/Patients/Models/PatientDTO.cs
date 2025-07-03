132026f46d34948130b90f7bb9aeb9e36fd8be76
auenaswaichik <ilpeshkur@gmail.com>
2025-07-03 14:24:19 +0300

    Adding patient dto

---
+++ ProfilesApi/Application/Patients/Models/PatientDTO.cs
@@ -0,0 +1,17 @@
+namespace Application.Patients.Models;
+
+public record PatientDTO
+{
+    public string? FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime DateBirth { get; set; }
+    public Guid ProfileId { get; set; }
+    public PatientDTO(string? firstName, string? lastName, DateTime dateBirth, Guid profileId)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        DateBirth = dateBirth;
+        ProfileId = profileId;
+    }
+};
