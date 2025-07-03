95aaf7f7f3556da9ed9a468f9c0f7c976131b169
auenaswaichik <ilpeshkur@gmail.com>
2025-07-03 10:47:02 +0300

    Fixing global exception handler

--- ProfilesApi/Api/Middleware/GlobalExceptionHandlerMiddleware.cs
+++ ProfilesApi/Api/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -21,7 +21,7 @@ public class GlobalExceptionHandlerMiddleware
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex.Message, "Unhabdled exception");
+            _logger.LogError(ex, ex.Message);
             await HandleExceptionAsync(httpContext, ex);
         }
     }
