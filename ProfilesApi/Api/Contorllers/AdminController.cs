2ed9826db9163c9dc7ae49fa1890aed242772470
auenaswaichik <ilpeshkur@gmail.com>
2025-07-09 15:52:06 +0300

    Adding admin controller

---
+++ ProfilesApi/Api/Contorllers/AdminController.cs
@@ -0,0 +1,64 @@
+using Application.Admins.Models;
+using Application.Admins.UseCases.CreateAdmin;
+using Application.Admins.UseCases.DeleteAdmin;
+using Application.Admins.UseCases.GetAllAdmins;
+using Application.Admins.UseCases.GetAdminById;
+using Application.Admins.UseCases.UpdateAdmin;
+using Domain.Entities.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/admins")]
+public class AdminController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AdminController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedList<AdminDTO>>> GetAdmins([FromQuery] int pageIndex, [FromQuery] int pageSize)
+    {
+        var query = new GetAllAdminsQuery() {PageIndex = pageIndex, PageSize = pageSize};
+
+        var response = await _mediator.Send(query);
+
+        return Ok(response.Items);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AdminDTO>> GetAdminById(Guid id)
+    {
+        var query = new GetAdminByIdQuery() { Id = id };
+        
+        return Ok(await _mediator.Send(query));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AdminDTO>> CreateAdmin([FromBody] CreateAdminCommand command)
+    {
+        return Ok(await _mediator.Send(command));
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<AdminDTO>> UpdateAdmin([FromBody] UpdateAdminCommand command)
+    {
+        return Ok(await _mediator.Send(command));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<AdminDTO>> DeleteAdmin(Guid id)
+    {
+        var command = new DeleteAdminCommand() { Id = id };
+
+        await _mediator.Send(command);
+        
+        return Ok();
+    }    
+
+}
