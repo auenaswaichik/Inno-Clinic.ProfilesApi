f8e69726360b7202fb907b76892f6463f4a67399
auenaswaichik <ilpeshkur@gmail.com>
2025-07-09 14:21:15 +0300

    Implementing find by id for admins repository

--- ProfilesApi/Domain/Interfaces/IRepositories/IAdminRepository.cs
+++ ProfilesApi/Domain/Interfaces/IRepositories/IAdminRepository.cs
@@ -1,6 +1,8 @@
+using Domain.Entities;
+
 namespace Domain.Interfaces.IRepositories;
 
-public interface IAdminRepository
+public interface IAdminRepository : IGenericRepository<Admin>
 {
-
+    public Task<Admin> GetByIdAsync(Guid id, CancellationToken token);
 }
--- ProfilesApi/Infrastructure/Repositories/AdminRepository.cs
+++ ProfilesApi/Infrastructure/Repositories/AdminRepository.cs
@@ -7,8 +7,14 @@ namespace Infrastructure.Repositories;
 
 public class AdminRepository : GenericRepository<Admin>, IAdminRepository
 {
+    private readonly ProfilesApiDbContext _context;
     public AdminRepository(ProfilesApiDbContext context) : base(context)
     {
- 
+        _context = context;
+    }
+
+    public async Task<Admin> GetByIdAsync(Guid id, CancellationToken token)
+    {
+        return await _context.Admins.FirstOrDefaultAsync(m => m.Id == id);
     }
 }
